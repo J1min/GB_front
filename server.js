37822b8abf8b8cf104de6748c413c0f07e361442
J1min <jmhong101506@gmail.com>
2021-09-01 22:23:50 +0900

    서버 공부 Ver 1.1

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
-});
+const MongoClient = require("mongodb").MongoClient;
+
+var db;
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.rjy7d.mongodb.net/SERVER?retryWrites=true&w=majority",
+  { useUnifiedTopology: true },
+  function (에러, client) {
+    if (에러) return console.log(에러);
+    db = client.db("todoapp");
+    app.listen(8080, function () {
+      console.log("listening on 8080");
+    });
+  }
+);
 
 app.get("/", function (요청, 응답) {
   응답.sendFile(__dirname + "/index.html");
@@ -16,6 +27,11 @@ app.get("/write", function (요청, 응답) {
 });
 
 app.post("/add", function (요청, 응답) {
-  console.log(요청.body);
+  db.collection("post").insertOne(
+    { 제목: 요청.body.title, 날짜: 요청.body.date },
+    function (에러, 결과) {
+      console.log("저장완료");
+    }
+  );
   응답.send("전송완료");
 });
