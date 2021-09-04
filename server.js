468b82c3cb6e0440e68485840a81fb27debb8bb9
J1min <jmhong101506@gmail.com>
2021-09-05 00:42:32 +0900

    수정삭제를 위한 DB 내 개별ID 저장기능 추가

--- server.js
+++ server.js
@@ -37,12 +37,28 @@ app.get("/list", function (요청, 응답) {
 });
 
 app.post("/add", function (요청, 응답) {
-  db.collection("post").insertOne(
-    { 제목: 요청.body.title, 날짜: 요청.body.date },
+  응답.send("전송완료");
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
     function (에러, 결과) {
-      if (에러) return 에러;
-      console.log("저장완료");
+      console.log(결과.totalPost);
+      var 총게시물갯수 = 결과.totalPost;
+
+      db.collection("post").insertOne(
+        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
+        function (에러, 결과) {
+          if (에러) return 에러;
+          console.log("저장완료");
+
+          db.collection("counter").updateOne(
+            { name: "게시물갯수" },
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) return console.log(에러);
+            }
+          );
+        }
+      );
     }
   );
-  응답.send("전송완료");
 });
