9129410dbbeaa2e6d7a9ad7af32aea0aa1a38e1b
J1min <jmhong101506@gmail.com>
2021-09-07 21:39:25 +0900

    오류 해결

--- Python/Test.py
+++ Python/Test.py
@@ -1,6 +1,6 @@
 
-f = input("섭씨 온도를 입력하세요 (단위 : C)\n")
-f = int(f)
-c = (f-32) / 1.8
-c = int(c)
-print(f'섭씨 온도 {f}C 의 화씨 온도는 {c}F 입니다')
+instr = input("문자열:")
+
+re_instr = instr[::-1]
+print('{}'.format(instr)) 
+print('{}'.format(re_instr))
