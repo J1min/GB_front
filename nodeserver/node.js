cdede12986ee93b3f197cbe1fdfd05a5446e053b
J1min <jmhong101506@gmail.com>
2021-09-14 09:02:40 +0900

    새로운 토이프로젝트

---
+++ nodeserver/node.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 8081;
+app.set("view engine", "ejs");
+const MongoClient = require("mongodb").MongoClient;
+
+var db;
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.rjy7d.mongodb.net/SERVER?retryWrites=true&w=majority",
+  { useUnifiedTopology: true },
+  function (에러, client) {
+    if (에러) return console.log(에러);
+    db = client.db("myweb");
+    app.listen(port, function () {
+      console.log("listening on 8081");
+    });
+  }
+);
+
+// 제목: req.body.title, 날짜: req.body.date
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+  req.send("전송 끝");
+});
+
+app.get("/show", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+
+app.post("/add", function (req, res) {
+  res.send("전송 완료");
+
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
+    function (에러, 결과) {
+      console.log(결과.totalPost);
+      var 총게시물갯수 = 결과.totalPost;
+
+      db.collection("post").insertOne(
+        { _id: 총게시물갯수 + 1, 제목: req.body.title, 날짜: req.body.date },
+        function (에러, 결과) {
+          console.log("저장완료");
+        }
+      );
+      db.collection("counter").updateOne(
+        { name: "게시물갯수" },
+        { $inc: { totalPost: 1 } }
+      );
+    }
+  );
+});
