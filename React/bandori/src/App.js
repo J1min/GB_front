5fcb1f07141330b6fa3334b12b8f1678c8ce8fbf
J1min <jmhong101506@gmail.com>
2021-09-27 22:23:14 +0900

    뱅드림

---
+++ React/bandori/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+//import "./App.css";
+
+function App() {
+  const name = "아냥";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>어서와 {name}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
