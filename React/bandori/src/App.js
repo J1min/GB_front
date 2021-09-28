deb66e5335636573aefaf5af9cd6bc4b26b85fa9
J1min <jmhong101506@gmail.com>
2021-09-28 22:29:03 +0900

    props 장인을 꿈꾼다

--- React/bandori/src/App.js
+++ React/bandori/src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
-import count from "./counter";
-
+import MyName from "./MyName";
 class App extends Component {
-  
   render() {
-    return (
-      <div>
-        <h1>카운터</h1>
-        <div>값 : {this.state.number}</div>
-        <button onClick={count.state.handleIncrease}>증가</button>
-        <button onClick={count.state.handleDecrease}>감소</button>
-      </div>
-    );
+    return <MyName name="방붕이" age="123"></MyName>;
   }
 }
 
--- React/bandori/src/MyName.js
+++ React/bandori/src/MyName.js
@@ -1,17 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
+// class MyName extends Component {
+//   render() {
+//     return (
+//       <div>
+//         안녕하세요! 제 이름은 <b>{this.props.name}</b> 입니다.
+//       </div>
+//     );
+//   }
+// }
 class MyName extends Component {
   render() {
     return (
-      <div>
-        안녕하세요! 제 이름은 <b>{this.props.name}</b> 입니다.
-      </div>
+      <Fragment>
+        <div>
+          안녕하세요 제 이름은 <b>{this.props.name}</b> 입니다
+        </div>
+        <div>
+          저는 <b>{this.props.age}</b> 살이에여
+        </div>
+      </Fragment>
     );
   }
 }
 
+// const MyName = ({ name, age }) => {
+//   return (
+//     <div>
+//       안녕하세요 제 이름은 {name}, {age}살 입니다
+//     </div>
+//   );
+// };
 MyName.defaultProps = {
-  name: "name이라는 props가 비었을때 나오는 이름",
+  name: "기모띠띠띠",
+  age: 32,
 };
-
 export default MyName;
