5e0ad45cbb9ffd754095ca25b5850a66a53b64e2
J1min <jmhong101506@gmail.com>
2021-10-04 18:32:12 +0900

    input 이벤트리스너 복습

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,27 +1,35 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Component, Fragment } from "react";
+import { Component } from "react";
 
 class App extends Component {
   state = {
     id: "",
     pw: "",
   };
-  showData = (e) => {
+
+  showId = (e) => {
     this.setState({
-      [e.target.id]: e.target.value,
+      id: e.target.value,
+    });
+  };
+
+  showPw = (e) => {
+    this.setState({
+      pw: e.target.value,
     });
   };
 
   render() {
     return (
       <form>
-          <input name="id" onChange={this.showData} value={this.state.id} />
-          <input name="pw" onChange={this.showData} value={this.state.pw} />
-          <div>
-            {this.state.id}
-            {this.state.pw}
-          </div>
+        <div>
+          <input type="text" name="id" onChange={this.showId} />
+        </div>
+        <div>
+          <input type="text" name="pw" onChange={this.showPw} />
+        </div>
+        <div>{this.state.id}</div>
+        <div>{this.state.pw}</div>
       </form>
     );
   }
