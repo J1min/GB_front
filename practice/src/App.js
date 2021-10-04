16626b1112daf95ea53abd0006a04f8b7232c4d6
J1min <jmhong101506@gmail.com>
2021-10-05 08:35:29 +0900

    vue에도 눈이 돌아가버려

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,37 +1,11 @@
-import "./App.css";
-import { Component } from "react";
-
-class App extends Component {
-  state = {
-    id: "",
-    pw: "",
-  };
-
-  showId = (e) => {
-    this.setState({
-      id: e.target.value,
-    });
-  };
-
-  showPw = (e) => {
-    this.setState({
-      pw: e.target.value,
-    });
-  };
-
+import React, { Component } from "react";
+import Eraser from "./components/Eraser";
+export default class App extends Component {
   render() {
     return (
-      <form>
-        <div>
-          <input type="text" name="id" onChange={this.showId} />
-        </div>
-        <div>
-          <input type="text" name="pw" onChange={this.showPw} />
-        </div>
-        <div>{this.state.id}</div>
-        <div>{this.state.pw}</div>
-      </form>
+      <div>
+        <Eraser></Eraser>
+      </div>
     );
   }
 }
-export default App;
