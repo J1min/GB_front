e682860586cbd42b5de5c5f9e0f6f15ffdccf069
J1min <jmhong101506@gmail.com>
2021-10-05 17:25:56 +0900

    시험 끝나고는 이제 VUE 공부해야겠다

---
+++ practice/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>
+      뱅드림걸파
+    </h1>
+    <HelloWorld msg="뱅드림 걸즈밴드파티 화이팅" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./assets/components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
