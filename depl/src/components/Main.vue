aecdc5af9aca00d45aa15efbb76c216521d981a0
J1min <jmhong101506@gmail.com>
2021-10-11 10:43:11 +0900

    qodemfla

---
+++ depl/src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>마루야마 아야 화이팅</h1>
+    <button
+      type="button"
+      class="btn btn-outline-primary"
+      onclick="location.href='/start'"
+    >
+      Primary
+    </button>
+    <img src="https://codingapple1.github.io/vue/room0.jpg" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ depl/src/components/OnError.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>
+    에러 남 ㅅㄱ
+  </h1>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
