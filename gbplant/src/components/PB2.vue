05588f838ca65dc1e9c6169a26593da83165aa7d
J1min <jmhong101506@gmail.com>
2021-10-12 20:38:10 +0900

    야기

--- gbplant/src/components/PB2.vue
+++ gbplant/src/components/PB2.vue
@@ -6,7 +6,14 @@
         style="width:300px; height:300px"
       />
     </router-link>
-    <h2  class="mt-5 mb-5">토마토</h2>
+    <p>
+      적정 재배온도: 25~30C <br>
+      수확시기: 약 4~5개월 소요 <br>
+      물 주는 시기: 흙 표면이 건조해 졌을 때. (2~3일) <br>
+      주의사항: 영상 5C 이하로 떨어지면 성장이 잘 되지
+      않는다.
+    </p>
+    <h2 class="mt-5 mb-5">토마토</h2>
     <router-link to="/my/plant1">
       <button type="button" class="btn btn-outline-success">시작하기</button>
     </router-link>
--- gbplant/src/components/PB3.vue
+++ gbplant/src/components/PB3.vue
@@ -6,6 +6,13 @@
         src="https://i.ibb.co/G0yv4JX/image.png"
         style="width:300px; height:300px"
     /></router-link>
+    <p>
+      적정 재배온도: 13~18도<br>
+      수확시기: 약 2개월 소요 <br>
+      물 주는 시기: 흙 표면이 건조해 졌을 때. (2~3일) <br>
+      주의사항: 아침 공복에 비트만 갈아 만든 주스를 마시면 
+      안좋음.
+    </p>
     <h2  class="mt-5 mb-5">비트</h2>
     <router-link to="/my/plant1">
       <button type="button" class="btn btn-outline-success">시작하기</button>
