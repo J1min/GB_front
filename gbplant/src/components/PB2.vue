7de151dcbf25549566bda7290794ea0589624a5f
J1min <jmhong101506@gmail.com>
2021-10-12 20:32:20 +0900

    뱅드리머

--- gbplant/src/components/PB2.vue
+++ gbplant/src/components/PB2.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-      <router-link to="/choose/plant1">
-        <img
-          id="sang"
-          src="https://i.ibb.co/PjBwXxx/image.png"
-          style="width:100px; height:100px"
-        />
-      </router-link>
-      <h2>토마토</h2>
-    </div>
+    <router-link to="/choose/plant1">
+      <img
+        src="https://i.ibb.co/PjBwXxx/image.png"
+        style="width:300px; height:300px"
+      />
+    </router-link>
+    <h2  class="mt-5 mb-5">토마토</h2>
+    <router-link to="/my/plant1">
+      <button type="button" class="btn btn-outline-success">시작하기</button>
+    </router-link>
+  </div>
 </template>
 <script>
 export default {};
--- gbplant/src/components/PB3.vue
+++ gbplant/src/components/PB3.vue
@@ -2,11 +2,14 @@
   <div>
     <router-link to="/choose/plant2">
       <img
-        id="sang"
+        class="mt-5"
         src="https://i.ibb.co/G0yv4JX/image.png"
-        style="width:100px; height:100px"
+        style="width:300px; height:300px"
     /></router-link>
-    <h2>비트</h2>
+    <h2  class="mt-5 mb-5">비트</h2>
+    <router-link to="/my/plant1">
+      <button type="button" class="btn btn-outline-success">시작하기</button>
+    </router-link>
   </div>
 </template>
 <script>
