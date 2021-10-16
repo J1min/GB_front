f45700b2c7574f6db119c152a71c5680f4be8b77
J1min <jmhong101506@gmail.com>
2021-10-16 11:51:22 +0900

    파이썬시험 ㄺㄴ

--- Python/index.py
+++ Python/index.py
@@ -1,8 +1,8 @@
-num=0
-i=0
-while i<100:
-  i = i + 1
-  num = num + i
-  
+data = '''@dlfmadla, Yea Chan, Kim
+@mss3380, Min Sae, Kim
+@sty0507, Tae Young, Seo
+@kwonminseo051214, Min Seo, Kwon
+@DoHi0512, Dong Hun, Lee'''
+data.replace('@',' ')
+print(data)
 
-print(num)
