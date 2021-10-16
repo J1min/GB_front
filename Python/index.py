6f3d022eba61be0b4411ea18c42422ff9076a5b7
J1min <jmhong101506@gmail.com>
2021-10-16 23:14:46 +0900

    풀스택은언제하지

--- Python/index.py
+++ Python/index.py
@@ -1,8 +1,7 @@
-data = '''@dlfmadla, Yea Chan, Kim
-@mss3380, Min Sae, Kim
-@sty0507, Tae Young, Seo
-@kwonminseo051214, Min Seo, Kwon
-@DoHi0512, Dong Hun, Lee'''
-data.replace('@',' ')
-print(data)
-
+print('hello world')
+print('hello', end="")
+print('world')
+print('hello', 'world')
+print('hello' + 'world')
+print('hello')
+print('world')
