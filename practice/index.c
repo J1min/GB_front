d19320fa2486aecc04ed41f35399edc0bd711c94
J1min <jmhong101506@gmail.com>
2021-10-27 16:25:16 +0900

    일단 그 뭐냐 파이썬 시험이 끝났어요 ㅎㅎ

--- practice/index.c
+++ practice/index.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-int main() {
-  int n=1;
-  printf("%d",n);
+int main()
+{
+    int i,j,n=4,temp,key;
+    int list[4]={5,4,2,1};
+    for(i=1; i<n; i++)
+    {
+        key=list[i];
+        for(j=i-1; j>=0 && list[j]>key; j--)
+        {
+            list[j+1]=list[j];
+        }
+        list[j+1]=key;
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("%d ",list[i]);
+    }
+    return 0;
 }
