5e38ce7ea7090869509cb7ec37031f7d94d4439b
J1min <jmhong101506@gmail.com>
2021-10-29 23:31:21 +0900

    초심으로 돌아간 노드js

---
+++ apple/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.listen(port, function () {
+  console.log(`listening on ${port}`);
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/pet", function (req, res) {
+  res.sendFile(__dirname + "/pet.html");
+});
+
+app.get("/beauty", function (req, res) {
+  res.sendFile(__dirname + "/beauty.html");
+});
