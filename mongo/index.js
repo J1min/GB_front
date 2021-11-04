35dad176a094f312434a0ee17afcf279c3573cc5
J1min <jmhong101506@gmail.com>
2021-11-04 11:51:44 +0900

    스스로몽고디비

---
+++ mongo/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
+
+var db;
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.rjy7d.mongodb.net/mongoose?retryWrites=true&w=majority",
+  function (err, client) {
+    if (err) return console.log(err);
+    db = client.db("mongoose");
+    app.listen(port, function () {
+      console.log(`listening on ${port}`);
+    });
+  }
+);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+
+app.get("/list", function (req, res) {
+  console.log(result);
+  res.render("list.ejs", { data: result });
+});
+
+app.post("/add", function (req, res) {
+  res.send("전송 완료");
+  var info = { 제목: req.body.title, 날짜: req.body.date };
+  db.collection("data").insertOne(info, function () {
+    console.log("전송완료입니다람쥐썬더");
+  });
+});
