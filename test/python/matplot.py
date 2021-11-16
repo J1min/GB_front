810b8805a591f48f6df2b859c65e21d5a4a0abda
J1min <jmhong101506@gmail.com>
2021-11-16 09:21:38 +0900

    matplot 한글 성공ㅋㅋ

---
+++ test/python/matplot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+plt.rcParams['axes.unicode_minus'] = False 
+
+x = [2, 3, 6, 7, 9]
+#y = [50, 20, 30, 40, 10]
+plt.plot(x)
+#plt.plot(x,y)
+plt.title("한글이안나와요")
+plt.show()
